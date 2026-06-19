4d684e496b585ddc330a5d049274a59bfac7ee05
Pedro-glitch-afk <1242116@isep.ipp.pt>
2026-06-19 22:04:33 +0100

    Limpeza

--- Guião W2 Paulo/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getDB(): SQLite3 {
-    $caminho = __DIR__ . '/../data/casaMusica.db';
-    $db = new SQLite3($caminho);
-    $db->exec('PRAGMA foreign_keys = ON;');
-    return $db;
-}
--- Guião W2 Paulo/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'sessao.php';
-session_destroy();
-header('Location: ../index.html');
-exit;
